cae5a821ee1d1830b0384dc65b99128843aed33e
Murat Umutlu <muratumutlu@gmail.com>
2020-06-08 12:00:29 +0300

    settings layout updated

--- src/layouts/SettingsLayout.js
+++ src/layouts/SettingsLayout.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+
 export default function SettingsLayout(props) {
   return (
     <div className="container text-light" style={{ marginTop: "15vh" }}>
+      <Navbar />
       {props.children}
     </div>
   );
